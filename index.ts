1124b0088a708ef503e6ada8b9644b20e818e1c3
James Abney <james.abney@gmail.com>
2022-07-15 13:57:11 -0500

    Move stream exports to root index

--- index.ts
+++ index.ts
@@ -3,11 +3,30 @@ import { Row } from "./lib/row"
 import { StreamData as StreamDataClass } from "./lib/stream-data"
 
 export * from "./lib/types"
-export * from "./lib/stream"
 export { Header } from "./lib/header"
 export { Row } from "./lib/row"
-export interface StreamData extends StreamDataClass {}
 
-console.log(
-    new Row({ index: 0, header: new Header([]), meta: new Map(), storage: new Map(), types: new Map(), values: [] })
-)
+export { csv, CsvOptions } from "./lib/stream/read/csv"
+export { each } from "./lib/stream/write/each"
+export { print } from "./lib/stream/write/print"
+export { run } from "./lib/stream/write/run"
+export { write } from "./lib/stream/write/write"
+export { Writer, WriteProjectFn } from "./lib/stream/write/writer"
+export { aggregate, AggregateFn } from "./lib/stream/transform/aggregate"
+export { append, AppendFn } from "./lib/stream/transform/append"
+export { audit, AuditFn } from "./lib/stream/transform/audit"
+export { constant, ConstantFn } from "./lib/stream/transform/constant"
+export { describe, DescribeItem } from "./lib/stream/transform/describe"
+export { exclude } from "./lib/stream/transform/exclude"
+export { filter, FilterFn } from "./lib/stream/transform/filter"
+export { map, MapFn } from "./lib/stream/transform/map"
+export { order } from "./lib/stream/transform/order"
+export { rename } from "./lib/stream/transform/rename"
+export { report, ReportFn } from "./lib/stream/transform/report"
+export { result, ResultFn } from "./lib/stream/transform/result"
+export { search, SearchFn } from "./lib/stream/transform/search"
+export { sample, SampleFn } from "./lib/stream/transform/sample"
+export { select } from "./lib/stream/transform/select"
+export { store, StoreFn } from "./lib/stream/transform/store"
+
+export interface StreamData extends StreamDataClass {}
