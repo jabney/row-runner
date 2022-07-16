bb8468292aaee3930bbc830bafdc4cd445b52bcc
James Abney <james.abney@gmail.com>
2022-07-16 07:39:11 -0500

    Refactor csv reader

--- lib/stream/read/csv.ts
+++ lib/stream/read/csv.ts
@@ -4,66 +4,57 @@ import { createReadStream } from "fs"
 import { rowExtractor } from "../../row-extractor"
 import { Header } from "../../header"
 import { Row } from "../../row"
-import { DoneFn } from "../../types"
+import { DoneFn, MetaMap, TypesMap } from "../../types"
+import { StreamData } from "../../stream-data"
 
 /**
  * Read a csv file and convert rows to a stream of values.
  */
 export const csv = (path: string, options: Partial<CsvOptions> = {}, done?: DoneFn): Readable => {
+    const start = process.hrtime()
     const opts = new CsvOptions(options)
     const rl = createInterface({ input: createReadStream(path) })
-    const start = process.hrtime()
-    const stream = new Readable({ objectMode: true, read: () => {} })
-    const types = new Map()
-    const meta = new Map()
+    const rs = new Readable({ objectMode: true, read: () => {} })
+    const types: TypesMap = new Map()
+    const meta: MetaMap = new Map()
 
     let header: Header
     let row: Row
     let index = 0
 
-    if (opts.hasHeader) {
-        rl.on("line", (line) => {
-            const extract = rowExtractor(opts.separator)
-            const values = extract(line)
-            const storage = new Map()
+    rl.on("line", (line) => {
+        const extract = rowExtractor(opts.separator)
+        const values = extract(line)
+        const storage = new Map()
 
-            if (index === 0) {
+        if (index === 0) {
+            if (opts.hasHeader) {
                 header = new Header(values)
-                stream.emit("header", header)
             } else {
-                row = new Row({ index: index - 1, values, header, types, meta, storage })
-                stream.emit("row", row)
-                stream.push({ index: index - 1, row, meta, opts })
-            }
-            index += 1
-        })
-    } else {
-        rl.on("line", (line) => {
-            const extract = rowExtractor(opts.separator)
-            const values = extract(line)
-            const storage = new Map()
-
-            if (index === 0) {
                 header = new Header(values.map((_, i) => i.toString()))
-                stream.emit("header", header)
+                row = new Row({ index: 0, values, header, types, meta, storage })
+                const data: StreamData = { index: 0, row, meta, types }
+                rs.push(data)
             }
-            row = new Row({ index, values, header, types, meta, storage })
-            stream.emit("row", row)
-            stream.push({ index, row, meta, opts })
-            index += 1
-        })
-    }
+        } else {
+            const i = opts.hasHeader ? index - 1 : index
+            row = new Row({ index: i, values, header, types, meta, storage })
+            const data: StreamData = { index: i, row, meta, types }
+            rs.push(data)
+        }
+        index += 1
+    })
 
     rl.on("close", () => {
-        stream.push(null)
+        rs.push(null)
         const [s, ns] = process.hrtime(start)
         const ms = s * 1e3 + Math.round(ns / 1e6)
         const data = { ms, rows: row.index + 1 }
-        stream.emit("done", data)
+        rs.emit("done", data)
         done?.(data)
     })
 
-    return stream
+    return rs
 }
 
 export class CsvOptions {
