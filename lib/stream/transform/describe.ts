44d0aa08ec696f6a19328478cda1411338bcc7f6
James Abney <james.abney@gmail.com>
2022-07-15 22:52:02 -0500

    Change describe item type

--- lib/stream/transform/describe.ts
+++ lib/stream/transform/describe.ts
@@ -2,16 +2,17 @@ import { Stream } from "stream"
 import { StreamData } from "../../stream-data"
 import { ColumnSpec, DataType } from "../../types"
 
-export type DescribeItem = [col: ColumnSpec | ColumnSpec[], type: DataType]
+export type DescribeItem = { cols: ColumnSpec | ColumnSpec[]; type: DataType }
 
-export const describe = (desc: DescribeItem[]) => {
+export const describe = (desc: DescribeItem | DescribeItem[]) => {
     return new Stream.Transform({
         objectMode: true,
         transform: (data: StreamData, _, next) => {
             if (data.index === 0) {
-                desc.forEach(([spec, type]) => {
-                    const cols = new Set(data.row.header.selectIndices(spec))
-                    const columns = data.row.header.columns.filter((_, i) => cols.has(i))
+                const descriptions = Array.isArray(desc) ? desc : [desc]
+                descriptions.forEach(({ cols, type }) => {
+                    const set = new Set(data.row.header.selectIndices(cols))
+                    const columns = data.row.header.columns.filter((_, i) => set.has(i))
                     columns.forEach((key) => data.row.types.set(key, type))
                 })
             }
