800d229a129e2e5cb20cf088ff1f764ae5b2793e
James Abney <james.abney@gmail.com>
2022-07-17 12:21:30 -0500

    Allow printing to writable

--- lib/stream/write/print.ts
+++ lib/stream/write/print.ts
@@ -1,15 +1,19 @@
+import { EOL } from "os"
+import { Writable } from "stream"
 import { DoneFn } from "../../types"
 import { Writer } from "./writer"
+
+const isConsole = (writer?: typeof console | Writable): writer is typeof console => {
+    return writer === console
+}
 
 /**
  * Write rows of data to stdout.
  */
-export const print = (done?: DoneFn) => {
+export const print = (done?: DoneFn, writer: typeof console | Writable = console) => {
     return new Writer(
         null,
-        (row) => {
-            console.log(row.asObject())
-        },
+        (row) => void (isConsole(writer) ? console.log(row.asObject()) : writer.write(row.toJSON() + EOL)),
         done
     )
 }
